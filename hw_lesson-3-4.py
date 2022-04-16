0c61f5180e8b03c37a47c8c108200321b100a866
VladimirKushnarev <k.vladimir.r@gmail.com>
2022-04-16 23:52:16 +0200

    fixed bugs in hw_lesson-3-4.py

--- hw_lesson-3-4.py
+++ hw_lesson-3-4.py
@@ -1,6 +1,6 @@
 # Task 3-4
 
-def rais_to_power(var, power):
+def raise_to_power(var, power):
     if var == 0:
         print('Неправильно введены данные, x не может быть равен 0.')
         return None
@@ -9,13 +9,14 @@ def rais_to_power(var, power):
     if power > 0 or var < 0:
         print('Неправильно введены данные.')
         return None
-    res = 1 / var
+    inverse = 1 / var
+    res = inverse
     for i in range(abs(power) - 1):
-        res *= res
+        res *= inverse
     return res
 
 
 if __name__ == '__main__':
     x = float(input('Введите действительное положительное число x: '))
     y = int(input('Введите целое отрицательное число y: '))
-    print(rais_to_power(x, y))
+    print(raise_to_power(x, y))
